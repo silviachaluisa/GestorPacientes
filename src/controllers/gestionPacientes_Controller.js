f292df052f850779b1b8c51d20c3cb8aca5df3f5
silviachaluisa <veronica-14-1998@hotmail.com>
2024-06-06 10:50:19 -0500

    Archivos

---
+++ src/controllers/gestionPacientes_Controller.js
@@ -0,0 +1,41 @@
+
+// crear una nueva cita de paciente
+
+const crearPacienteController = (req,res) =>{
+
+}
+
+// visualizar las citas de los pacientes por ID
+
+const verPacienteController = (req,res) =>{
+    const pacienteId = req.params.id
+}
+
+// visualizar todos los pacientes que se encuentran registrados 
+
+const verTodosPacientesController = (req,res) =>{
+
+}
+
+// Actualizar la ficha de los pacientes que ya se encontraban registrados
+
+
+const actualizarPacienteController = (req,res) =>{
+
+}
+
+// borrar ficha del paciente por ID
+
+const borrarPacienteController = (req,res) =>{
+
+}
+
+export {
+    crearPacienteController,
+    verPacienteController,
+    verTodosPacientesController,
+    actualizarPacienteController,
+    borrarPacienteController
+}
+
+
---
+++ src/models/gestionPacientes.js
@@ -0,0 +1,37 @@
+const pacientes ={
+
+    // para poder crear un nuevo paciente 
+    crearPacienteModelo(){
+         
+    }
+    ,
+
+    // para poder ver un paciente en especifico 
+
+    verPacienteModelo(){
+
+    }
+    ,
+
+    // para poder ver todos los pacientes 
+
+    verTodosPacientesModelo(){
+
+    }
+    ,
+
+    // para actualizar paciente 
+
+    actualizarPacienteModelo(){
+
+    }
+    ,
+
+    // para borrar paciente 
+
+    borrarPacienteModelo(){
+
+    }
+}
+
+export default pacientes
